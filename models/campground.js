f6dfca3ceaef6636611480023347b3aeea0a2f62
ryansereno <ryansereno@protonmail.com>
2022-07-01 13:51:11 -0400

    added review functionality

--- models/campground.js
+++ models/campground.js
@@ -1,20 +1,37 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Review = require("./review")
+const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
-    name:{
-        type:String
+    name: {
+        type: String,
     },
-    image:{
-        type:String
+    image: {
+        type: String,
     },
-    price:{
-        type:Number
+    price: {
+        type: Number,
     },
-    description:{
-        type:String
+    description: {
+        type: String,
     },
-    location:String
+    location: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        },
+    ],
+});
+
+CampgroundSchema.post('findOneAndDelete', async function(document){
+    if (document){
+        await Review.remove({
+            _id:{
+                $in: document.reviews //remove the reviews for which their id's are in this document
+            }
+        })
+    }
 })
 
-module.exports = mongoose.model('Campground', CampgroundSchema)
+module.exports = mongoose.model("Campground", CampgroundSchema);
