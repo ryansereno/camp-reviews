538c14535e9d33938ea6d90c2ba7ae48b7074f00
ryansereno <ryansereno@protonmail.com>
2022-07-12 18:59:44 -0400

    updted cloud storage directory

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -9,8 +9,10 @@ cloudinary.config({
 
 const storage = new CloudinaryStorage({
     cloudinary,
+    params: {
     folder: 'CampReview',
     allowedFormats: ['jpeg','png','jpg']
+    }
 })
 
 module.exports = {
