7f8ccc4f5ba62bd16983a4b934fcdaaf1a74a910
ryansereno <ryansereno@protonmail.com>
2022-07-16 13:32:52 -0400

    added image upload to edit page and enpoints

--- seeds/index.js
+++ seeds/index.js
@@ -1,44 +1,54 @@
 //this file is not required by any other code, but simply acts as a function if data needs to be seeded into the app
 
 const mongoose = require("mongoose");
-const cities = require('./cities')
+const cities = require("./cities");
 const Campground = require("../models/campground");
-const {places, descriptors} = require('./seedHelpers')
+const { places, descriptors } = require("./seedHelpers");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
-    useNewUrlParser: true,
-    //useCreateIndex:true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  //useCreateIndex:true,
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-    console.log("Database connected");
-})
+  console.log("Database connected");
+});
 
-const sample = (array) =>{
-    return array[Math.floor(Math.random() * array.length)]
+const sample = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
 };
 
-const seedDB = async()=>{ //reset database for test
-    await Campground.deleteMany({})
-    for (let i=0; i<50; i++){
-        const random1000 = Math.floor(Math.random() * 1000)
-        const price = Math.floor(Math.random() * 40) + 10
+const seedDB = async () => {
+  //reset database for test
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 40) + 10;
 
-        const newCampground = new Campground({
-            author:'62c6d5ea5f827344ea71c34e',
-            name:`${sample(descriptors)} ${sample(places)}`,
-            location:`${cities[random1000].city}, ${cities[random1000].state}`,
-            image: 'https://source.unsplash.com/collection/627564/400x400',
-            description:"Elit harum dolor quod quae earum Dolor quis ipsam corporis facere consectetur soluta. Et fuga tempore dolore laudantium est praesentium maxime minus! Voluptatem sequi possimus magni numquam quos eius, est",
-            price: price
-        }) 
-        await newCampground.save()
-    }
-}
-seedDB()
-.then(() =>{
-    mongoose.connection.close()
-})
+    const newCampground = new Campground({
+      author: "62c6d5ea5f827344ea71c34e",
+      name: `${sample(descriptors)} ${sample(places)}`,
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      images: [
+        {
+          url: "https://res.cloudinary.com/da4w19b6p/image/upload/v1657990458/CampReview/umvtdaptagll6gqngech.jpg",
+          filename: "CampReview/umvtdaptagll6gqngech",
+        },
+        {
+          url: "https://res.cloudinary.com/da4w19b6p/image/upload/v1657990457/CampReview/sufe9udyipk3rxu1ukxx.png",
+          filename: "CampReview/sufe9udyipk3rxu1ukxx",
+        },
+      ],
+      description:
+        "Elit harum dolor quod quae earum Dolor quis ipsam corporis facere consectetur soluta. Et fuga tempore dolore laudantium est praesentium maxime minus! Voluptatem sequi possimus magni numquam quos eius, est",
+      price: price,
+    });
+    await newCampground.save();
+  }
+};
+seedDB().then(() => {
+  mongoose.connection.close();
+});
